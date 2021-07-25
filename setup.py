bcf071365d225a3fbc2e6e91afdb5e176a1f1c6e
Theodikes <gvedichi@gmail.com>
2021-07-25 15:52:33 +0300

    Add developer requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Theodikes/jsmethods",
     install_requires=[],
+    test_require=["pytest"],
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python',
