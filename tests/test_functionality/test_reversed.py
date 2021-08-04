def2ad3f7a4b4c7c9cb513c2092ccb5eac6c44bc
Theodikes <gvedichi@gmail.com>
2021-08-04 20:05:58 +0300

    Add one test to 'reversed' method Compare our method with the result of reverse iteration over the array in a loop and transferring elements to another array

--- tests/test_functionality/test_reversed.py
+++ tests/test_functionality/test_reversed.py
@@ -1,3 +1,5 @@
+import random
+
 import listchaining
 from random import randint
 
@@ -26,5 +28,14 @@ def test_reversed_empty_array():
 def test_reversed_complex_array():
     test_array = [14, 1, [88, 99, 909, [98, 11]], "str", 34, ["str2", "str1", [list("str3"), 13, ["14", [19, 20], 16]]]]
     result_array = list(reversed(test_array))
+
+    assert test_array.reversed() == result_array
+
+
+def test_reversed_compare_to_reverse_with_cycle():
+    test_array = [randint(36456, 475665) for _ in range(100000)]
+    result_array = []
+    for index in range(len(test_array) - 1, -1, -1):
+        result_array.append(test_array[index])
 
     assert test_array.reversed() == result_array
